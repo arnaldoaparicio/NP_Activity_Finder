7e863705f9f7d5e81f0e4270f17d6e261795b646
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-08 10:40:44 -0400

    Add 'find_one_park' method

--- app/services/national_park_service.rb
+++ app/services/national_park_service.rb
@@ -1,18 +1,22 @@
 class NationalParkService
-
-    def self.conn 
-        url = "https://developer.nps.gov/api/v1/"
-        Faraday.new(url: url) do |faraday|
-            faraday.params['api_key'] = ENV['parks_key']
-        end
-
+  def self.conn
+    url = 'https://developer.nps.gov/api/v1/'
+    Faraday.new(url: url) do |faraday|
+      faraday.params['api_key'] = ENV['parks_key']
     end
+  end
 
-    def self.parks_by_state(state)
-        response = conn.get('parks') do |faraday|
-         faraday.params['stateCode'] = state
-        end
-        JSON.parse(response.body, symbolize_names: true)
+  def self.parks_by_state(state)
+    response = conn.get('parks') do |faraday|
+      faraday.params['stateCode'] = state
     end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
+  def self.find_one_park(park_code)
+    response = conn.get('parks') do |faraday|
+      faraday.params['parkCode'] = park_code
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
