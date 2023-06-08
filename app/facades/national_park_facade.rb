5535af228d31653fb7a16287131806fd26b104cb
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-08 10:40:18 -0400

    Add 'one_park' method

--- app/facades/national_park_facade.rb
+++ app/facades/national_park_facade.rb
@@ -1,10 +1,15 @@
 class NationalParkFacade
-
-    def self.get_parks_by_state(state)
-        search = NationalParkService.parks_by_state(state)
-        parks = search[:data]
-        parks.map do |park|
-            Park.new(park)
-        end
+  def self.get_parks_by_state(state)
+    search = NationalParkService.parks_by_state(state)
+    parks = search[:data]
+    parks.map do |park|
+      Park.new(park)
     end
+  end
+
+  def self.one_park(park_code)
+    single_park = NationalParkService.find_one_park(park_code)
+    park = single_park[:data][0]
+    Park.new(park)
+  end
 end
