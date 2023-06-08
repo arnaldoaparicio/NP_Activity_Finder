2be5bb86c7353bf888674558be4ddd887503a1b3
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-08 11:31:34 -0400

    Add test to search for one park

--- spec/services/national_parks_service_spec.rb
+++ spec/services/national_parks_service_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 require 'pry'
 
-RSpec.describe NationalParkService do 
-    it "returns all parks in a given state" do 
-        state = 'MD'
+RSpec.describe NationalParkService do
+  it 'returns all parks in a given state' do
+    state = 'MD'
 
-        search = NationalParkService.parks_by_state(state)
-        expect(search).to be_a Hash
-    end
+    search = NationalParkService.parks_by_state(state)
+    expect(search).to be_a Hash
+  end
 
+  it 'returns all events' do
+    park_code = 'cajo'
+
+    events = NationalParkService.find_all_park_events(park_code)
+    expect(events).to be_a Hash
+  end
 end
