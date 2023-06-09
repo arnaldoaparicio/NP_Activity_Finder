ced93054b8fb0e12d6e13fd42e4a0a56df091d2c
Wade Naughton <wadenaughton@gmail.com>
2023-06-09 09:24:43 -0400

    error handling

--- app/poro/event.rb
+++ app/poro/event.rb
@@ -4,17 +4,34 @@ class Event
 
   def initialize(data)
 
-    @location = data[:location]
+    @location = blank_location(data)
     @description = data[:description]
     @name = data[:title]
     @date = data[:dates] #iterate
     @time = data[:times] #iterate
     @free = data[:isfree]
     @id = data[:id]
-    @fee_info = data[:feeinfo]
+    # @fee_info = data[:feeinfo]
+    @fee_info = blank_event(data)
     @latitude = data[:latitude]
     @longitude = data[:longitude]
     @type = data[:types] #iterate
+  end
+
+  def blank_event(data)
+    if @fee_info == nil
+      @fee_info = "No fee information available."
+    else 
+      @fee_info = data[:feeinfo]
+    end
+  end
+
+  def blank_location(data)
+    if @location == nil
+      @location = "No location information available."
+    else 
+      @location = data[:location]
+    end
   end
   
 end
