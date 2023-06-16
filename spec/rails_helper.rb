ac5e476de05a3ca4f7f4732e42c3fe2b800c3ed8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 18:16:23 -0400

    Add vcr code to force encoding

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,5 +74,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.default_cassette_options = { re_record_interval: 7.days}
-  config.allow_http_connections_when_no_cassette = true
+  config.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
+  # config.allow_http_connections_when_no_cassette = true
 end
