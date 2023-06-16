5b6020f566bfcb5942c957fe761c7e3c12571d49
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 15:22:42 -0400

    Rename file

--- db/migrate/20230616181553_create_user_events.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserEvents < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_events do |t|
-      t.references :user, foreign_key: true
-      t.references :park, foreign_key: true
-      t.references :event, foreign_key: true
-    end
-  end
-end
