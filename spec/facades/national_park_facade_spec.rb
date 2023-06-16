185c399675a95e321a55caca730d4f191c76a7f5
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 16:56:19 -0400

    Adjust formatting

--- spec/facades/national_park_facade_spec.rb
+++ spec/facades/national_park_facade_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 require 'pry'
 require 'date'
 
-RSpec.describe NationalParkFacade do 
-    it "returns parks by state code" do 
+RSpec.describe NationalParkFacade do
+  it 'returns parks by state code' do
+    state = 'md'
+    search = NationalParkFacade.get_parks_by_state(state)
+    expect(search).to be_a Array
+  end
 
-        state = 'md'
-        search = NationalParkFacade.get_parks_by_state(state)
-        expect(search).to be_a Array
-    end
+  it 'returns events by park code' do
+    park_code = 'shen'
+    start = Date.today.to_s
+    finish = (Date.today + 7).to_s
 
-    it "returns events by park code" do 
-        park_code = 'shen'
-        start = Date.today.to_s
-        finish = (Date.today + 7).to_s
-        
-        search = NationalParkFacade.all_park_events(park_code, start, finish)
-    end
-
+    search = NationalParkFacade.all_park_events(park_code, start, finish)
+  end
 end
