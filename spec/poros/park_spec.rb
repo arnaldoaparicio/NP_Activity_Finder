127a12ce4046a4f3071e1f705aed2384742e551a
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 16:56:06 -0400

    Check attributes for new poro

--- spec/poros/park_spec.rb
+++ spec/poros/park_spec.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 RSpec.describe ' Park Poro' do
   it 'tests park attributes' do
-    
     data = {
       fullName: 'Test',
       description: 'This is a description',
@@ -57,9 +56,29 @@ RSpec.describe ' Park Poro' do
 
     }
 
-   
-      
     park = Park.new(data)
     expect(park).to be_a Park
+
+    expect(park.name).to eq('Test')
+    expect(park.description).to eq('This is a description')
+    expect(park.park_code).to eq('1')
+    expect(park.latitude).to eq('1234')
+    expect(park.longitude).to eq('456')
+    expect(park.phone_number).to eq('5555555555')
+    expect(park.email).to eq('bob@bob.com')
+    expect(park.entrance_fees).to eq([['$1.00', 'description'], ['$2.00', 'description2']])
+    expect(park.directions_website).to eq('www.www.com')
+    expect(park.closed_day).to eq('Christmas')
+    expect(park.operating_hours).to eq([
+                                         %w[Monday 9],
+                                         %w[Tuesday 9],
+                                         %w[Wednesday 9],
+                                         %w[Thursday 9],
+                                         %w[Friday 9],
+                                         %w[Saturday 9],
+                                         %w[Sunday 9]
+                                       ])
+    expect(park.address).to eq('123 test rd, Bear, DE 19701')
+    expect(park.photos).to eq([['wade4.jpg', 'wade2'], ['wade4.jpg', 'wade2']])
   end
 end
