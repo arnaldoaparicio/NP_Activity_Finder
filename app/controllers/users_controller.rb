1c6a3785f3c6bb5de54ce833f9915fb8bc5c8e37
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 18:18:12 -0400

    Fix formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,18 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-    def show
-        @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
   end
-  def create
 
+  def create
     @user = User.new(user_params)
     if @user.save
-        log_in @user
+      log_in @user
       redirect_to user_path(@user.id)
     else
       render :new
@@ -23,19 +23,19 @@ class UsersController < ApplicationController
 
   def login
     user = User.find_by(username: params[:username])
-  if user.nil?
-    render :login_form
-  elsif user.authenticate(params[:password])
-     session[:user_id] = user.id
-     redirect_to user_path(user)
-  else
-    render :login_form
-  end
-end
-
-  private 
-
-    def user_params
-        params.permit(:first_name, :last_name, :username, :state, :password, :password_confirmation)
+    if user.nil?
+      render :login_form
+    elsif user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :login_form
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :username, :state, :password, :password_confirmation)
+  end
 end
