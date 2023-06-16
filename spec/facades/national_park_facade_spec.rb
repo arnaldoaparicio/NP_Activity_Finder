0f5e6f999a3f99c67ccd8297abcc7af49a2a1b51
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 17:13:33 -0400

    Add test to check for one instance of Park

--- spec/facades/national_park_facade_spec.rb
+++ spec/facades/national_park_facade_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe NationalParkFacade do
     state = 'md'
     search = NationalParkFacade.get_parks_by_state(state)
     expect(search).to be_a Array
+
+    search.each do |s|
+      expect(s).to be_a Park
+    end
   end
 
   it 'returns events by park code' do
@@ -15,5 +19,18 @@ RSpec.describe NationalParkFacade do
     finish = (Date.today + 7).to_s
 
     search = NationalParkFacade.all_park_events(park_code, start, finish)
+
+    search.each do |s|
+      expect(s).to be_an Event
+    end
+  end
+
+  it 'returns one park by park code' do
+    park_code = 'shen'
+    single_park = NationalParkService.find_one_park(park_code)
+
+    park = Park.new(single_park[:data][0])
+
+    expect(park).to be_a Park
   end
 end
