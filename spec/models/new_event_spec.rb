40b5e871ea0b36a0ab7e3c2a1579ac21357251ec
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-16 16:56:36 -0400

    Rename file

---
+++ spec/models/new_event_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe NewEvent, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :location }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :time }
+    it { should validate_presence_of :free }
+    it { should validate_presence_of :event_code }
+    it { should validate_presence_of :fee_info }
+    it { should validate_presence_of :latitude }
+    it { should validate_presence_of :longitude }
+    it { should validate_presence_of :type }
+  end
+
+  # describe 'relationships' do
+  #     it {should have_many(:user_events)}
+  #     it {should have_many(:users).through(:user_events)}
+  # end
+end
