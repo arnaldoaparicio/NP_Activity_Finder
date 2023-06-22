dcdd28f2910ad5a30a47785514a555e1caa6d8a2
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-22 10:00:39 -0400

    Add new association

--- app/models/user_new_event.rb
+++ app/models/user_new_event.rb
@@ -2,8 +2,10 @@ class UserNewEvent < ApplicationRecord
 
     validates_presence_of :user_id
     validates_presence_of :new_event_id
+    validates_presence_of :new_park_id
 
     belongs_to :user
     belongs_to :new_event
+    belongs_to :new_park
 
 end
