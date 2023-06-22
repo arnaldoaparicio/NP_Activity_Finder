edba249b8dde35989f145b8bff5774f9d5f93c61
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-22 09:58:24 -0400

    Fix formatting

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -9,21 +9,17 @@ class ParksController < ApplicationController
     if NewPark.where(id: params[:id]).empty?
       @park = NationalParkFacade.one_park(params[:park_code])
 
-
       n = NewPark.new(address: @park.address, closed_day: @park.closed_day, description: @park.description,
-                                 directions_website: @park.directions_website, email: @park.email, entrance_fees: @park.entrance_fees,
-                                 latitude: @park.latitude, longitude: @park.longitude, name: @park.name,
-                                 operating_hours: @park.operating_hours, park_code: @park.park_code, phone_number: @park.phone_number, photos: @park.photos)
-       
-        n.save
-        @park = NewPark.find_by(id: n.id)
+                      directions_website: @park.directions_website, email: @park.email, entrance_fees: @park.entrance_fees,
+                      latitude: @park.latitude, longitude: @park.longitude, name: @park.name,
+                      operating_hours: @park.operating_hours, park_code: @park.park_code, phone_number: @park.phone_number, photos: @park.photos)
+
+      n.save
+      @park = NewPark.find_by(id: n.id)
     else
-     
+
       @park = NewPark.find(params[:id])
     end
-
-     
-
   end
 
   private
