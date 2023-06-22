20434c0ca6d98cdc8681cc80ff23750b27fb0e10
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-22 09:59:48 -0400

    Fix formatting

--- app/controllers/user_new_parks_controller.rb
+++ app/controllers/user_new_parks_controller.rb
@@ -1,25 +1,21 @@
 class UserNewParksController < ApplicationController
+  def new
+  end
 
-
-    def new
+  def create
+    @user = User.find(params[:user_id])
+    @park = NewPark.find_by(id: params[:park_id])
+    @user_new_park = UserNewPark.find_or_create_by(user_id: @user.id, new_park_id: @park.id)
+    if @user_new_park.save
+      redirect_to user_path(@user_new_park.user)
+    else
+      render :new
     end
+  end
 
-    def create 
-        @user = User.find(params[:user_id])
-        @park = NewPark.find_by(id: params[:park_id])
-        @user_new_park = UserNewPark.find_or_create_by(user_id: @user.id, new_park_id: @park.id)
-        if @user_new_park.save
-            redirect_to user_path(@user_new_park.user)
-        else
-            render :new
-        end
-    end
+  private
 
-
-private 
-
-    def user_new_park_params
-        params.require(:user_new_park).permit(:user_id, :new_park_id)
-    end
-
+  def user_new_park_params
+    params.require(:user_new_park).permit(:user_id, :new_park_id)
+  end
 end
