f64024ec03908a339d25e9eaf2c3c0b41fc0bfd0
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-04 11:58:56 -0400

    Fix formatting

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe MapService do
-    it "returns a list of businesses", :vcr do
-        location = "-77.6445079,39.3341151"
-        q = "bar"
-        circle = "-77.6445079,39.3341151,100000"
-        sort = "distance"
-        response = MapService.get_service(location, q, circle, sort)
-    end
+  it 'returns a list of businesses', :vcr do
+    location = '-77.6445079,39.3341151'
+    q = 'bar'
+    circle = '-77.6445079,39.3341151,100000'
+    sort = 'distance'
+    response = MapService.get_service(location, q, circle, sort)
+  end
 end
