a69128106e8e7a0c4c4983cbe75db7de0025e729
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-04 11:57:13 -0400

    Fix formatting

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,20 +1,18 @@
 class MapService
-
-    def self.conn
-        url = 'https://www.mapquestapi.com/search/v4/'
-        Faraday.new(url: url) do |faraday|
-            faraday.params['key'] = ENV['map_key']
-        end
+  def self.conn
+    url = 'https://www.mapquestapi.com/search/v4/'
+    Faraday.new(url: url) do |faraday|
+      faraday.params['key'] = ENV['map_key']
     end
+  end
 
-    def self.get_service(location, q, circle, sort)
-        response = conn.get('place?') do |faraday|
-            faraday.params['location'] = location
-            faraday.params['q'] = q
-            faraday.params['circle'] = circle
-            faraday.params['sort'] = sort
-        end
-        JSON.parse(response.body, symbolize_names: true)
+  def self.get_service(location, q, circle, sort)
+    response = conn.get('place?') do |faraday|
+      faraday.params['location'] = location
+      faraday.params['q'] = q
+      faraday.params['circle'] = circle
+      faraday.params['sort'] = sort
     end
-
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
