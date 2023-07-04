c4ae55bb0cb83c098fc89f818afa8b08b9fde274
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-04 11:55:30 -0400

    Fix formatting

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -1,11 +1,8 @@
 class MapFacade
-
-    def self.get_service(location, q, circle, sort)
-        search = MapService.get_service(location, q, circle, sort)
-        search[:results].map do |business|
-            Business.new(business)
-        end
+  def self.get_service(location, q, circle, sort)
+    search = MapService.get_service(location, q, circle, sort)
+    search[:results].map do |business|
+      Business.new(business)
     end
-
-
+  end
 end
