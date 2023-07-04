e1892ea8a01506dd72f8f0af78025915f9c8d838
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-04 11:58:19 -0400

    Fix formatting

--- spec/models/user_new_park_spec.rb
+++ spec/models/user_new_park_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserNewPark, type: :model do
-    it { should validate_presence_of :user_id }
-    it { should validate_presence_of :new_park_id }
-    it { should belong_to :user }
-    it { should belong_to :new_park }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :new_park_id }
+  it { should belong_to :user }
+  it { should belong_to :new_park }
 end
