2fb785c055217e59b949afbf4df52d6ed39fe75a
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-04 11:58:32 -0400

    Fix formatting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,19 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :password_digest }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :state }
+  end
 
-    describe "validations" do
-        it { should validate_presence_of :password_digest }
-        it { should validate_presence_of :first_name }
-        it { should validate_presence_of :last_name }
-        it { should validate_presence_of :username }
-        it { should validate_presence_of :state }
-    end
-    
-    describe "relationships" do
-        it {should have_many(:user_new_events)}
-        it {should have_many(:new_events).through(:user_new_events)}
+  describe 'relationships' do
+    it { should have_many(:user_new_events) }
+    it { should have_many(:new_events).through(:user_new_events) }
 
-        it {should have_many(:user_new_parks)}
-        it {should have_many(:new_parks).through(:user_new_parks)}
-    end
-
+    it { should have_many(:user_new_parks) }
+    it { should have_many(:new_parks).through(:user_new_parks) }
+  end
 end
