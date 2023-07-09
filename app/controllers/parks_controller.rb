cdabe2619d3fb0e41bf5f7de32ffc02026257985
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-09 19:23:40 -0400

    Add park code and state

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -6,6 +6,7 @@ class ParksController < ApplicationController
 
   def show
     @user = User.find_by(id: session[:user_id])
+    @state_info = { park_code: params[:park_code], state: params[:state] }
     if NewPark.where(id: params[:id]).empty? && NewPark.where(park_code: params[:park_code]).empty?
       @park = NationalParkFacade.one_park(params[:park_code])
       n = NewPark.new(address: @park.address, closed_day: @park.closed_day, description: @park.description,
@@ -21,9 +22,6 @@ class ParksController < ApplicationController
       @park = NewPark.find_by(id: params[:id]) || NewPark.find_by(park_code: params[:park_code])
       @weather = WeatherFacade.get_forecast(@park.latitude, @park.longitude)
     end
-
-  
-
   end
 
   def accessible_places
