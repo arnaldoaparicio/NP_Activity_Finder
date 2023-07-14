063e999bb24dba43a244e4cabb63ff8bb9f3f33b
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-14 17:56:17 -0400

    Add 'formatted_url' method

--- app/poros/alert.rb
+++ app/poros/alert.rb
@@ -3,12 +3,16 @@ class Alert
 
   def initialize(data)
     @id = data[:id]
-    @url = data[:url]
+    @url = formatted_url(data[:url])
     @title = data[:title]
     @description = data[:description]
     @category = data[:category]
     @last_index = data[:lastIndexedDate]
   end
 
+  def formatted_url(url)
+    return 'No link available.' if url.empty?
 
+    url
+  end
 end
