2699dd670ff8c4973bbcfb5022d19b81b9b9486b
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-14 17:53:12 -0400

    Remove pry

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,5 @@
 class AlertsController < ApplicationController
   def index
     @alerts = NationalParkFacade.get_alerts(params[:park_code], params[:state])
-    require 'pry'; binding.pry
   end
 end
